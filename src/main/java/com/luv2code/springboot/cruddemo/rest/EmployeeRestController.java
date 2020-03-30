39edf71c4d4de788cbde114aa7d3a3aa0ff05154
gmailbob <mianyangbob@gmail.com>
2020-03-30 19:10:52 -0400

    use service Interface to refactor code a little bit

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -7,22 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
 
-	// quick and dirty, inject employee dao using constructor injection
 	@Autowired
-	public EmployeeRestController(EmployeeDAO employeeDAO) {
-		this.employeeDAO = employeeDAO;
+	public EmployeeRestController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
 
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
-		return employeeDAO.findAll();
+		return employeeService.findAll();
 	}
 }
