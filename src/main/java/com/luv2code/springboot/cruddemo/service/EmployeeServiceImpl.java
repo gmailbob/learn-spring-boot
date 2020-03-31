e0f6fca69984ba586c2fb7849a774f6948b44556
gmailbob <mianyangbob@gmail.com>
2020-03-30 22:59:02 -0400

    finish demo with JPA implementation

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +16,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private EmployeeDAO employeeDAO;
 
 	@Autowired
-	public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+	// @Qualifier("employeeDAOJpaImpl") indicates which implementation to use
+	// since we've got two now and employeeDAOJpaImpl is the bean id
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
 		this.employeeDAO = employeeDAO;
 	}
 
