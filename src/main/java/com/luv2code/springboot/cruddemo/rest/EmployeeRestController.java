0dd814a7c7b32cf28cda978683f5b10322cb327f
gmailbob <mianyangbob@gmail.com>
2020-03-30 22:15:46 -0400

    add delete and put controller; finish of EntityManager+native Hibernate API

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,12 +38,28 @@ public class EmployeeRestController {
 		}
 		return employee;
 	}
-	
+
 	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee employee) {
 		// set id to 0 to enforce create new instead of doing update
 		employee.setId(0);
 		employeeService.save(employee);
 		return employee;
+	}
+
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+		employeeService.save(employee);
+		return employee;
+	}
+
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		Employee employee = employeeService.findById(employeeId);
+		if (employee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		employeeService.deleteById(employeeId);
+		return "Deleted employee id - " + employeeId;
 	}
 }
