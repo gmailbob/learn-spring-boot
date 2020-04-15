1e931addec24d4d32a77d88c0a8bba60c600a05d
gmailbob <mianyangbob@gmail.com>
2020-04-14 22:58:30 -0400

    thymeleaf update employee

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
@@ -36,12 +37,21 @@ public class EmployeeController {
 		model.addAttribute("employee", employee);
 		return "employees/employee-form";
 	}
-	
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+		// get employee from the service
+		Employee employee = employeeService.findById(id);
+		// set employee as a model attribute to pre-populate the form
+		model.addAttribute("employee", employee);
+		return "employees/employee-form";
+	}
+
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
 		// save to database
 		employeeService.save(employee);
-		
+
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/employees/list";
 	}
