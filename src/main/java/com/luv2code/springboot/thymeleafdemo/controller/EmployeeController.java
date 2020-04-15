4c79691eb6768b4da779c358822010a85593b11d
gmailbob <mianyangbob@gmail.com>
2020-04-14 22:41:57 -0400

    thymeleaf employees add

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -24,7 +26,23 @@ public class EmployeeController {
 	public String listEmployees(Model model) {
 		List<Employee> employees = employeeService.findAll();
 		model.addAttribute("employees", employees);
-		return "list-employees";
+		return "employees/list-employees";
 	}
 
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		// create model attribute to bind form data
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		// save to database
+		employeeService.save(employee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+	}
 }
--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
@@ -7,5 +9,8 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
+	
+	// add a method to sort by last name. Spring Data JPA will extract the method name and create corresponding query for us
+	public List<Employee> findAllByOrderByLastNameAsc(); // => from Employee order by lastName asc
 
 }
